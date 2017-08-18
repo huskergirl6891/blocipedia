c1dca5d00eb90229b3a63cc31d4e11caf3c331c6
Carissa Castro <carissabcastro@gmail.com>
2017-08-18 14:37:23 -0500

    Checkpoint 6 Seeding Data complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,35 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+5.times do
+  User.create!(
+  email:    Faker::Internet.unique.email,
+  password: Faker::Internet.password(8)
+  )
+end
+users = User.all
+
+# Create wikis
+50.times do
+  wiki = Wiki.create!(
+    user: users.sample,
+    title: Faker::Superhero.name,
+    body: Faker::ChuckNorris.fact,
+    private: false
+  )
+end
+wikis = Wiki.all
+
+# Create an admin user
+admin = User.create!(
+  email:    'admin@example.com',
+  password: 'helloworld',
+  role:     'admin'
+)
+
+# Create a standard user
+standard = User.create!(
+  email:    'carissabcastro@gmail.com',
+  password: 'helloworld'
+)
+
+puts "Seed finished"
+puts "#{User.count} users created"
+puts "#{Wiki.count} wikis created"
