20a8801447cc5175a3d54d4c9b6a4716d89d7ab8
Carissa Castro <carissabcastro@gmail.com>
2017-08-18 12:57:41 -0500

    Checkpoint 4 Wiki CRUD complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+
   devise_for :users
   get 'welcome/index'
 
   root 'welcome#index'
+
+  resources :wikis
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:user) { User.create!(email: "user@blocipedia.com", password: "password") }
+
+  describe "attributes" do
+    it "should have email attribute" do
+      expect(user).to have_attributes(email: user.email)
+    end
+
+    # it "responds to role" do
+    #   expect(user).to respond_to(:role)
+    # end
+    #
+    # it "responds to admin?" do
+    #   expect(user).to respond_to(:admin?)
+    # end
+    #
+    # it "responds to member?" do
+    #   expect(user).to respond_to(:member?)
+    # end
+  end
 end
